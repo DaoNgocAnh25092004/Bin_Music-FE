de84ee0e3be2d1455c79ff1e1641cd5bbe2f4f1c
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2025-03-29 20:53:31 +0700

    Complete redux user

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    id: '',
+    name: '',
+    email: '',
+    avatar: '',
+    role: '',
+    isLogin: false,
+};
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        updateUser: (state, action) => {
+            const { userId, name, email, avatar, role, isLogin } =
+                action.payload;
+
+            state.id = userId;
+            state.name = name;
+            state.email = email;
+            state.avatar = avatar;
+            state.role = role;
+            state.isLogin = isLogin;
+        },
+
+        logout: (state) => {
+            Object.assign(state, initialState);
+            localStorage.removeItem('user');
+        },
+    },
+});
+
+// Export the actions
+export const { updateUser, logout } = userSlice.actions;
+
+// Export the reducer
+export default userSlice.reducer;
