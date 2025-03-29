a152d79d252609f7b33710ec60c0a32dee84f85e
DaoNgocAnh25092004 <161846189+DaoNgocAnh25092004@users.noreply.github.com>
2025-03-29 19:53:20 +0700

    Complete router configuration feature

---
+++ src/config/routes.js
@@ -0,0 +1,24 @@
+const routes = {
+    // Page public
+    explore: '/',
+    library: '/library',
+    binChart: '/bin-chart',
+    radio: '/radio',
+    ratingNewMusic: '/rating-new-music',
+    topicAndCategory: '/topic-and-category',
+    top100: '/top-100',
+    page404: '*',
+
+    // Page: id
+    albumDetail: '/album/:albumId',
+
+    // Page admin
+    musicAdmin: '/admin/music',
+    musicAdminCreate: '/admin/music/create',
+    albumAdmin: '/admin/album',
+    albumAdminCreate: '/admin/album/create',
+    categoryAlbumAdmin: '/admin/album-category',
+    artist: '/admin/artist',
+};
+
+export default routes;
